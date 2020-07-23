829c18878f4b67f16ff4d505fa0fd97439c189f5
Bryn Dyment <bryndyment@gmail.com>
2020-07-23 21:32:33 +0000

    initial commit

---
+++ BigDot.pm
@@ -0,0 +1,17 @@
+package BigDot;
+
+use strict;
+use utf8;
+
+use constant DUMMY => 1;
+
+use vars qw (
+  $DUMMY
+  @DUMMY
+  %DUMMY
+  );
+
+BEGIN {
+  };
+
+1;
---
+++ bigdot.pl
@@ -0,0 +1,7 @@
+use strict;
+
+use Hoo;
+
+use BigDot;
+
+1;
